6405f143d33fd0022b56e3d78a764c2caaac6b9f
Anto231996 <tony2396@hotmail.it>
2022-03-28 16:16:27 +0200

    Aggiunto header

---
+++ src/components/IndexHeader.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="header-nav">
+    <div>
+      <img src="../assets/img/dc-logo.png" alt="main dc-logo">
+    </div>
+    <div>
+      <nav>
+        <ul>
+          <li v-for="(element, index) in navBarLink" :key="index">
+            <a href="#" :class="{active: element.current}">{{element.name}}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'IndexHeader',
+ data: function() {
+   return {
+     navBarLink:[
+      {
+        name:"characters",
+        url: "#",
+        current: false
+      },
+
+      {
+        name:"comics",
+        url: "#",
+        current: true
+      },
+
+      {
+        name:"movies",
+        url: "#",
+        current: false
+      },
+
+      {
+        name:"tv",
+        url: "#",
+        current: false
+      },
+
+      {
+        name:"games",
+        url: "#",
+        current: false
+      },
+
+      {
+        name:"collectibles",
+        url: "#",
+        current: false
+      },
+
+      {
+        name:"videos",
+        url: "#",
+        current: false
+      },
+
+      {
+        name:"fans",
+        url: "#",
+        current: false
+      },
+
+      {
+        name:"news",
+        url: "#",
+        current: false
+      },
+
+      {
+        name:"shop",
+        url: "#",
+        current: false
+      }
+
+     ]
+   }
+ },
+}
+</script>
+
+<style scoped lang="scss">
+  div.header-nav{
+    height: 15vh;
+    width: 70%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+
+    li{
+      display: inline;
+      margin-left: 1.7rem;
+      text-decoration: none;
+      list-style: none;
+      text-transform: uppercase;
+      font-weight: 600;
+      font-size: .8rem;
+
+      a{
+        text-decoration: none;
+        color: #212121;
+      }
+
+      a.active, 
+      a:hover{
+        color: #137beb;
+      }
+    }
+  }
+</style>
---
+++ src/components/IndexMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-content">
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'IndexMain',
+}
+</script>
+
+<style scoped lang="scss">
+  div.main-content{
+    height: 15vh;
+    background-color: #212121;
+  }
+</style>
