8e1ba65128d82c1df458809ebad1446c0ece1182
Anto231996 <tony2396@hotmail.it>
2023-09-19 19:00:37 +0200

    some fix

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,30 @@
 <template>
   <div id="app">
-
     <HeaderIndex />
     <MainCurrentSeries />
     <MainInfo />
     <FooterIndex />
-    
   </div>
 </template>
 
 <script>
-import HeaderIndex from './components/HeaderIndex.vue';
-import MainCurrentSeries from './components/MainCurrentSeries.vue';
-import MainInfo from './components/MainInfo.vue';
-import FooterIndex from './components/FooterIndex.vue';
+import HeaderIndex from "./components/HeaderIndex.vue";
+import MainCurrentSeries from "./components/MainCurrentSeries.vue";
+import MainInfo from "./components/MainInfo.vue";
+import FooterIndex from "./components/FooterIndex.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HeaderIndex, 
-    MainCurrentSeries, 
+    HeaderIndex,
+    MainCurrentSeries,
     MainInfo,
-    FooterIndex
-  }
-}
+    FooterIndex,
+  },
+};
 </script>
 
 <style lang="scss">
-
 @import "./assets/scss/style.scss";
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
-
-div#app{
-  min-width: 1800px;
-}
-
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap");
 </style>
--- src/components/MainInfo.vue
+++ src/components/MainInfo.vue
@@ -1,65 +1,64 @@
 <template>
   <div class="main-info">
     <div class="wrapper">
-      <div>
-        <a href="#">
-          <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-          <h4>digital comics</h4>
-        </a>
-      </div>
-      <div>
-        <a href="#">
-          <img src="../assets/img/buy-comics-merchandise.png" alt="">
-          <h4>dc merchandise</h4>
-        </a>
-      </div>
-      <div>
-        <a href="#">
-          <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-          <h4>subscription</h4>
-        </a>
-      </div>
-      <div>
-        <a href="#">
-          <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-          <h4>comic shop locator</h4>
-        </a>
-      </div>
-      <div>
-        <a href="#">
-          <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-          <h4>dc power visa</h4>
-        </a>
-      </div>
+      <a href="#">
+        <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+        <h4>digital comics</h4>
+      </a>
+
+      <a href="#">
+        <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+        <h4>dc merchandise</h4>
+      </a>
+
+      <a href="#">
+        <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+        <h4>subscription</h4>
+      </a>
+
+      <a href="#">
+        <img src="../assets/img/buy-comics-shop-locator.png" alt="" style="width: 35px;" />
+        <h4>comic shop locator</h4>
+      </a>
+
+      <a href="#">
+        <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+        <h4>dc power visa</h4>
+      </a>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MainInfo',
-}
+  name: "MainInfo",
+};
 </script>
 
 <style scoped lang="scss">
-  div.main-info{
-    height: 160px;
-    background-color: #0880f8;
+div.main-info {
+  height: 160px;
+  background-color: #0880f8;
 
-    div.wrapper > div a{
-      display: flex;
-      align-items: center;
-    }
-
-    div.wrapper > div a img{
-      width: 50px;
-      margin-right: 1rem;
-    }
-
-    div.wrapper > div a h4{
-      text-transform: uppercase;
-      color: white;
-      font-weight: normal;
-    }
+  div.wrapper {
+    gap: 20px;
   }
+
+  div.wrapper > a {
+    display: flex;
+    align-items: center;
+  }
+
+  div.wrapper > a img {
+    height: 100%;
+    width: 50px;
+    margin-right: 1rem;
+  }
+
+  div.wrapper > a h4 {
+    text-transform: uppercase;
+    color: white;
+    font-weight: normal;
+  }
+}
 </style>
